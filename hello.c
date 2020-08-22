fa2f5bb0930af3e4e17c75c350765ba3c6c02185
Taegan Jacobs <tjacobs5@huskers.unl.edu>
2020-08-22 00:35:43 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Taegan Jacobs
+ * Date: 2020/08/21
+ * 
+ * A simple hello world program
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     print("Taegan Jacobs\n");
+     
+     return 0;
+ }
+ 

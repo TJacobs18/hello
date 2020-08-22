1afcddb5b00c0cb83a5f4ef9b47c4693ed71a5b4
Taegan Jacobs <tjacobs5@huskers.unl.edu>
2020-08-22 21:59:37 +0000

    added my major to the output

--- hello.c
+++ hello.c
@@ -1,16 +1,17 @@
 /**
  * Author: Taegan Jacobs
  * Date: 2020/08/21
- * 
+ *
  * A simple hello world program
  */
  #include <stdlib.h>
  #include <stdio.h>
- 
+
  int main(int argc, char **argv) {
-     
-     print("Taegan Jacobs\n");
-     
+
+     printf("Taegan Jacobs!\n");
+
+     printf("Undeclared\n");
+
      return 0;
  }
- 
